e2b2eaace27bbeefe53d2c00a2c7f4eba5a3ff01
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-02-09 19:32:59 -0600

    problem 1

---
+++ to_do1.js
@@ -0,0 +1,14 @@
+// set & swap
+setSwap()
+
+function setSwap() {
+    let myNumber = 42
+    let myName = "Jenny"
+    let temp = myNumber
+    myNumber = myName
+    myName = temp
+    temp = myName
+    myName = myNumber
+    myNumber = temp
+
+}
